8dac813808edf0edc18f36f374db00af4120f97a
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-04-24 02:14:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
+  
   
 }
 
